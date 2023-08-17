35b5f2e6e56b5f9444949438b7e3177f5a85303d
chelseey <chelsey_anderson@outlook.com>
2023-08-18 00:52:30 +1000

    Css

--- src/components/codeBackground.js
+++ src/components/codeBackground.js
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { fetchAndDisplayCode } from '../utils/fetchCode.js';
 import Typed from 'typed.js';
+import { animate } from 'framer-motion';
 
 import "../css/dracula-prism.css";
 
 const CodeBackground = ({ pathToFile }) => {
     const [highlightedCode, setHighlightedCode] = useState('');
     const [lineNumbers, setLineNumbers] = useState('');
+    const codeContainerRef = useRef(null);
 
     useEffect(() => {
         fetchAndDisplayCode(pathToFile).then(code => {
@@ -24,41 +26,54 @@ const CodeBackground = ({ pathToFile }) => {
                 showCursor: true,
                 contentType: 'html',
             };
-    
+
+            const handleScroll = () => {
+                const container = codeContainerRef.current;
+                const contentHeight = container.scrollHeight;
+                const visibleHeight = container.offsetHeight;
+
+                if (contentHeight > visibleHeight) {
+                    const scrollAmount = container.scrollTop + 30; // Smooth scrolling instead of jumping to the end
+
+                    animate(container.scrollTop, scrollAmount, {
+                        duration: 0.5,
+                        onUpdate(value) {
+                            container.scrollTop = value;
+                        },
+                    });
+                }
+            };
+
             // Start with the first line number
             document.querySelector('.line-numbers').textContent = '1';
-    
+
             const codeTyped = new Typed('.language-javascript code', codeOptions);
-    
-            // Observe the content of our code box
+
             const observer = new MutationObserver(() => {
                 const typedContent = document.querySelector('.language-javascript code').textContent;
                 const typedLines = typedContent.split('\n').length;
                 const currentLineNumbers = document.querySelector('.line-numbers').textContent.split('\n').length;
-    
+
                 if (typedLines > currentLineNumbers) {
                     for (let i = currentLineNumbers + 1; i <= typedLines; i++) {
                         document.querySelector('.line-numbers').textContent += '\n' + i;
                     }
+                    handleScroll();  // Only scroll when new line is added
                 }
             });
-    
+
             const config = { childList: true, characterData: true, subtree: true };
             observer.observe(document.querySelector('.language-javascript code'), config);
-    
+
             return () => {
                 codeTyped.destroy();
                 observer.disconnect();
             };
         }
     }, [highlightedCode, lineNumbers]);
-    
-    
-    
-    
 
     return (
-        <div id="code-background">
+        <div id="code-background" ref={codeContainerRef}>
             <pre className="line-numbers"></pre>
             <pre className="language-javascript">
                 <code className="language-javascript"></code>
--- src/components/scrollingContainer.js
+++ src/components/scrollingContainer.js
@@ -1,63 +1,89 @@
-import React from 'react';
-import { Link, Element, Events, scrollSpy } from 'react-scroll';
+import React, { useRef, useState, useEffect } from 'react';
+import { Element } from 'react-scroll';
 import { motion } from 'framer-motion';
 
 const containerVariants = {
-    hidden: { opacity: 0, y: '-100vh' },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
-    exit: { opacity: 0, y: '100vh', transition: { duration: 0.5 } }
-  };
-  
-  const contentVariants = {
+    hidden: { opacity: 0, x: '-100vw' },
+    visible: { opacity: 1, x: 0, transition: { duration: 0.5 } },
+    exit: { opacity: 0, x: '100vw', transition: { duration: 0.5 } }
+};
+
+const contentVariants = {
     hidden: { scale: 0.8, opacity: 0 },
     visible: { scale: 1, opacity: 1, transition: { duration: 0.5 } }
-  };  
-  
-
+};
 
 function ScrollingContainer({ contentBlocks }) {
-  React.useEffect(() => {
-    Events.scrollEvent.register('begin', function() {
-      console.log('begin', arguments);
-    });
+    const [scrollPosition, setScrollPosition] = useState(0);
+    const containerRefs = useRef([]);
 
-    Events.scrollEvent.register('end', function() {
-      console.log('end', arguments);
-    });
+    useEffect(() => {
+    const onScroll = () => {
+        setScrollPosition(window.scrollY);
+        
+        // Trigger a re-render to adjust scales of all blocks
+        containerRefs.current.forEach((ref, index) => {
+            const scale = calculateScale(index);
+            ref.style.transform = `scale(${scale})`;
+        });
+    };
 
-    scrollSpy.update();
+    window.addEventListener('scroll', onScroll);
 
     return () => {
-      Events.scrollEvent.remove('begin');
-      Events.scrollEvent.remove('end');
+        window.removeEventListener('scroll', onScroll);
     };
-  }, []);
-
-  return (
-    <div className="scrolling-container">
-
-      {contentBlocks.map((content, index) => (
-    <Element name={`block-${index}`} key={index}>
-        <motion.div 
-            className="container"
-            initial="hidden"
-            animate="visible"
-            exit="exit"
-            variants={containerVariants}
-        >
-            <div className="content-box"> {/* This is the new box */}
-                <motion.div variants={contentVariants}>
-                    {content}
-                </motion.div>
-            </div>
-        </motion.div>
-    </Element>
-))}
+}, []);
 
 
+    const calculateScale = (index) => {
+      const ref = containerRefs.current[index];
+      if (!ref) return 0.5;
+  
+      const topDistance = ref.getBoundingClientRect().top;
+      const containerHeight = ref.getBoundingClientRect().height;
+      const windowHeight = window.innerHeight;
+  
+      // Calculate the distance from the center of the container to the center of the viewport
+      const containerCenter = topDistance + (containerHeight / 2);
+      const distanceToCenter = Math.abs(windowHeight / 2 - containerCenter);
+  
+      // Normalize the distance to a value between 0 and 0.5 based on its proximity to the viewport center
+      const scaleDifference = distanceToCenter / (windowHeight / 2) * 0.5;
+      const scale = 1 - scaleDifference;
+  
+      return Math.max(scale, 0.5);
+  }
+  
+  
 
-    </div>
-  );
+    return (
+        <div className="scrolling-container">
+            <div style={{ height: '100vh' }}></div> {/* Space before first container */}
+            
+            {contentBlocks.map((content, index) => (
+                <Element name={`block-${index}`} key={index}>
+                    <motion.div
+                        className="container"
+                        initial="hidden"
+                        animate="visible"
+                        exit="exit"
+                        variants={containerVariants}
+                        ref={el => containerRefs.current[index] = el}
+                        style={{ scale: calculateScale(index) }}
+                    >
+                        <div className="content-box">
+                            <motion.div variants={contentVariants}>
+                                {content}
+                            </motion.div>
+                        </div>
+                    </motion.div>
+                </Element>
+            ))}
+
+            <div style={{ height: '100vh' }}></div> {/* Space after last container */}
+        </div>
+    );
 }
 
 export default ScrollingContainer;
